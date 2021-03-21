4061e2019edb0545c0b24899a1fc094e95ef8e43
Lauren Emick <laurenemick6@gmail.com>
2021-03-21 15:32:20 -0700

    adds nav, dropdown, and side panel

---
+++ backend/frontend/src/components/Nav.js
@@ -0,0 +1,34 @@
+import React from 'react';
+
+import { makeStyles } from '@material-ui/core/styles';
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import IconButton from '@material-ui/core/IconButton';
+import MenuIcon from '@material-ui/icons/Menu';
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    flexGrow: 1,
+  },
+  menuButton: {
+    marginRight: theme.spacing(2),
+  }
+}));
+
+const Nav = () => {
+  const classes = useStyles();
+
+  return (
+    <div className={classes.root} color="primary">
+      <AppBar position="static">
+        <Toolbar style={{backgroundColor: "#1d3557"}}>
+          <IconButton edge="start" className={classes.menuButton} color="inherit" aria-label="menu">
+            <MenuIcon />
+          </IconButton>
+        </Toolbar>
+      </AppBar>
+    </div>
+  );
+}
+
+export default Nav; 
